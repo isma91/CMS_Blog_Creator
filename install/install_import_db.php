0d8c263471d58c57b02f3fa8b8943c14175ff159
isma91 <ismaydogmus@gmail.com>
2015-12-16 15:11:14 +0100

    ajout de la requete ajax qui affiche les derniers blogs + 2 articles et ajouts d'un champs dans la bdd

--- install/install_import_db.php
+++ install/install_import_db.php
@@ -35,6 +35,7 @@ try {
 			) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 		CREATE TABLE IF NOT EXISTS `comments` (
 			`id` int(11) NOT NULL AUTO_INCREMENT,
+			`post_id` int(11) NOT NULL,
 			`user_id` int(11) NOT NULL,
 			`title` varchar(255) NOT NULL,
 			`content` text NOT NULL,
--- media/js/home.js
+++ media/js/home.js
@@ -2,8 +2,10 @@
 /*jslint devel : true*/
 /*global $, document, this*/
 $(document).ready(function(){
-	var i;
+	var i, j, k;
 	i = 0;
+	j = 0;
+	k = 0;
 	get_last_six_blogs();
 	setInterval(get_last_six_blogs, 60000);
 	function get_last_six_blogs() {
@@ -14,13 +16,23 @@ $(document).ready(function(){
 				$("div#last_six_blogs").html('<div class="jumbotron"><h2 class="blog_name">No Blog yet !!</h2><div class="mui-panel"></div></div>');
 			} else {
 				for (i = 0; i < data.blog_id.length; i = i + 1) {
-					document.getElementById('last_six_blogs').innerHTML = document.getElementById('last_six_blogs').innerHTML + '<div class="jumbotron resume_last_six_blog"><h2><a href="?blog=' + data.slug[i] + '">' + data.blog_name[i] + '</a></h2><h3><a href="?profile=' + data.user_id[i] +  '">' + data.username[i] + '</a></h3><div class="mui-panel"><h3>blog content</h3></div></div>';
+					document.getElementById('last_six_blogs').innerHTML = document.getElementById('last_six_blogs').innerHTML + '<div class="jumbotron resume_last_six_blog"><h2><a href="?blog=' + data.slug[i] + '">' + data.blog_name[i] + '</a></h2><h3><a href="?profile=' + data.user_id[i] +  '">' + data.username[i] + '</a></h3><div id="' + data.slug[i] +'"></div></div>';
 				}
-				console.log(data.blog_id);
-				console.log(data.blog_name);
-				console.log(data.slug);
-				console.log(data.user_id);
-				console.log(data.username);
+				for (j = 0; j < data.slug.length; j = j + 1) {
+					$.getJSON('api/?blog=' + data.slug[j] + '&limit=2', function (data) {
+						if (typeof data.articles !== "undefined") {
+							for (k = 0; k < data.articles.length; k = k + 1) {
+								if (data.articles[k].content.length > 15) {
+									data.articles[k].content = data.articles[k].content.substr(0, 15);
+									data.articles[k].content = data.articles[k].content + "...";
+								}
+								document.getElementById(data.slug).innerHTML = document.getElementById(data.slug).innerHTML + '<div class="mui-panel"><h3 class="post_title">' + data.articles[k].title + '</h3><div class="mui-divider"></div><p>' + data.articles[k].content + '</p><div class="mui-divider"></div><i>' + data.articles[k].created_at + '</i></div>';
+							}
+						} else {
+							document.getElementById(data.slug).innerHTML = '<div class="mui-panel"><h3 class="post_title">Not posts in this blog yet !!</h3></div>';
+						}
+					});
+				}
 			}
 		});
 	}
