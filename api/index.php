09aa69029b100dbd6581821c47cf5bb0ac041e97
isma91 <ismaydogmus@gmail.com>
2015-12-17 17:50:20 +0100

    update api

--- api/index.php
+++ api/index.php
@@ -7,6 +7,7 @@ use controllers\PostsController;
 use controllers\CommentsController;
 use controllers\CategoriesController;
 use controllers\MailsController;
+use conntrollers\UsersController;
 
 if (isset($_GET['blog'])) {
 	$limit = (isset($_GET['limit']) && (is_int($_GET['limit']) || is_numeric($_GET['limit']))) ? $_GET['limit'] : 10;
