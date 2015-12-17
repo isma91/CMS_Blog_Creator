a925630f8c1f4a387454bb2e22ec8894d7b81491
Raph <rbleuzet@gmail.com>
2015-12-17 15:35:09 +0100

    categoriescontroller add get method

--- controllers/CategoriesController.php
+++ controllers/CategoriesController.php
@@ -1,28 +1,19 @@
 <?php
 namespace controllers;
 
-use models\Category;
 use models\Database;
+use models\Category;
 class CategoriesController extends Category
 {
-	public function create ($name)
-	{
-		$bdd = new Database('home');
-
-		$create = $bdd->getBdd()->prepare('INSERT INTO categories (name) VALUES (:name)');
-		$create->bindParam(':name', $name);
-		$create->execute();
-		return true;
-	}
-
-	public function getAll ()
+	public function get ()
 	{
 		$bdd = new Database('home');
 
 		$get = $bdd->getBdd()->prepare('SELECT id, name FROM categories WHERE active = 1');
 		$get->execute();
 
-		$all = $get->fetchAll(\PDO::FETCH_ASSOC);
-		return $all;
+		$all = $get->fetchAll(\PDO::FETCG_ASSOC);
+
+		return json_encode($all);
 	}
 }
